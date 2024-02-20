91808afe9aadc82b6761818fcc2ebecbb3f367ac
nbolikcoulon <73346412+nbolikcoulon@users.noreply.github.com>
2024-02-19 23:21:56 -0500

    Update MonteCarlo.py
    Update MINOTAUR

--- lib/MonteCarlo.py
+++ lib/MonteCarlo.py
@@ -9,35 +9,35 @@
 from random import uniform
 from multiprocessing import Pool
 
-from lib import FigOutputs as FigOut
+# from lib import FigOutputs as FigOut
+import FigOutputs as FigOut
 
 import numpy as np
 import emcee
 from math import exp, log
-
+import sys
 
 import Parameters as ParamFile
 
 
-
-
 #################################################### Scaling factor Intensities ####################################################
-def ScalingFactor(SimulatedIntensity, IntensityRelaxometry):
-    scaling = np.asarray([[0.0] for B in IntensityRelaxometry])
-    for B in range(len(IntensityRelaxometry)):
+def ScalingFactor(SimulatedIntensity, IntensityRelaxometry, ErrRelaxometry, AA):
+    scaling = {}
+    
+    for exp_num in IntensityRelaxometry.keys():
         ScalingNum = 0.0
         ScalingDen = 0.0
-        for VC in range(len(IntensityRelaxometry[B])):
-            if IntensityRelaxometry[B][VC][1] != "NA":
-                ScalingNum += IntensityRelaxometry[B][VC][1]*SimulatedIntensity[B][VC]/(IntensityRelaxometry[B][VC][2])**2
-                ScalingDen += (SimulatedIntensity[B][VC]/IntensityRelaxometry[B][VC][2])**2
-        scaling[B] = ScalingNum/ScalingDen
+        for vc in IntensityRelaxometry[exp_num][AA].keys():
+            if IntensityRelaxometry[exp_num][AA][vc] != "NA":
+                ScalingNum += IntensityRelaxometry[exp_num][AA][vc]*SimulatedIntensity[exp_num][vc]/(ErrRelaxometry[exp_num][AA][vc])**2
+                ScalingDen += (SimulatedIntensity[exp_num][vc]/ErrRelaxometry[exp_num][AA][vc])**2
+        scaling[exp_num] = ScalingNum/ScalingDen
             
     return scaling
 
 
 #################################################### lnlike ####################################################
-def lnlike(X, IntensityRelaxometry, RelaxData, B0HF, tauc, OtherInputs, magfield, Increment, FieldListUp, FieldListDown, ExperimentNumber, WTHF, d25, LFtimes, B0LFields, PropFunction):       #X is a vector contaning the parameters of the spectral density funciton
+def lnlike(X, AA, IntensityRelaxometry, ErrRelaxometry, RelaxData, RelaxErr, tauc, OtherInputs, magfield, Increment, FieldList, Delays, SetUp, B0LFields, PropFunction):       #X is a vector contaning the parameters of the spectral density funciton
     
     RelaxFunc = ParamFile.ImportFunc()
     
@@ -45,54 +45,46 @@ def lnlike(X, IntensityRelaxometry, RelaxData, B0HF, tauc, OtherInputs, magfield
     
     expLnF = exp(2.0*X[-1])
     
-    for Func in range(len(RelaxData)):
-        for B in range(len(B0HF[Func])):
-            model_real = RelaxFunc[Func](B0HF[Func][B], X[:-1], tauc, OtherInputs)[0]
-            model = model_real
-            
-            err = RelaxData[Func][B][2]
-
-            inv_sigma2 = err**2 + model**2 * expLnF
-            dist = -1.0/2.0 * ((RelaxData[Func][B][1] - model)**2 / inv_sigma2 + log(2.0*np.pi*inv_sigma2))
+    for Func in RelaxData.keys():
+        for B in RelaxData[Func].keys():
+            try:
+                exp_data = RelaxData[Func][B][AA]
+                exp_err = RelaxErr[Func][B][AA]
+                model = RelaxFunc[Func](B, X[:-1], tauc, OtherInputs[AA])
                 
-            distCalc += dist
-            
+                inv_sigma2 = exp_err**2 + model**2 * expLnF
+                dist = -1.0/2.0 * ((exp_data - model)**2 / inv_sigma2 + log(2.0*np.pi*inv_sigma2))
 
-    SimulatedInt = PropFunction(X[:-1], tauc, OtherInputs, magfield, Increment, FieldListUp, FieldListDown, ExperimentNumber, WTHF, d25, LFtimes, B0LFields, ParamFile.PositionAuto)
-    
+                distCalc += dist
+            except:
+                pass
 
-    #scaling factor
-    scaling = ScalingFactor(SimulatedInt, IntensityRelaxometry)
-            
-    for B in range(len(IntensityRelaxometry)):
-        Nvc = len(list(filter(lambda x: x!='NA', np.asarray(IntensityRelaxometry[B])[:,1])))
-        for VC in range(len(IntensityRelaxometry[B])):
-            if IntensityRelaxometry[B][VC][1] != "NA":
-                model_real = scaling[B]*SimulatedInt[B][VC]
-                model = model_real
-                
-                err = IntensityRelaxometry[B][VC][2]
+    SimulatedInt = PropFunction(X[:-1], tauc, OtherInputs[AA], magfield, Increment, FieldList, Delays, SetUp, B0LFields, ParamFile.PositionAuto)
+    scaling = ScalingFactor(SimulatedInt, IntensityRelaxometry, ErrRelaxometry, AA)
     
-                inv_sigma2 = err**2 + model**2 * expLnF
-                dist = -1.0/2.0 * ((IntensityRelaxometry[B][VC][1] - model)**2 / inv_sigma2 + log(2.0*np.pi*inv_sigma2))
+    for exp_num in SetUp.keys():
+        N_vc = len(list(filter(lambda x: x!='NA', np.asarray(list(IntensityRelaxometry[exp_num].values())))))
+        for vc in SetUp[exp_num]['vc']:
+            if IntensityRelaxometry[exp_num][AA][vc] != 'NA':
+                exp_data = IntensityRelaxometry[exp_num][AA][vc]
+                exp_err = ErrRelaxometry[exp_num][AA][vc]
+                model = scaling[exp_num]*SimulatedInt[exp_num][vc]
+            
+                inv_sigma2 = exp_err**2 + model**2 * expLnF
+                dist = -1.0/2.0 * ((exp_data - model)**2 / inv_sigma2 + log(2.0*np.pi*inv_sigma2))
                     
-                distCalc += dist/Nvc
-            
+                distCalc += dist / N_vc
                 
     return distCalc
 
-
-
-    
     
 #################################################### lnprob ####################################################
-def lnprob(X, IntensityRelaxometry, RelaxData, B0HF, tauc, OtherInputs, magfield, Increment, FieldListUp, FieldListDown, ExperimentNumber, WTHF, d25, LFtimes, B0LFields, PropFunction):
+def lnprob(X, AA, IntensityRelaxometry, ErrRelaxometry, RelaxData, RelaxErr, tauc, OtherInputs, magfield, Increment, FieldList, Delays, SetUp, B0LFields, PropFunction):
     lp = ParamFile.Cons(X)
     if lp != 0.0:
         return -np.inf
     else:
-        return lp + lnlike(X, IntensityRelaxometry, RelaxData, B0HF, tauc, OtherInputs, magfield, Increment, FieldListUp, FieldListDown, ExperimentNumber, WTHF, d25, LFtimes, B0LFields, PropFunction)
-    
+        return lp + lnlike(X, AA, IntensityRelaxometry, ErrRelaxometry, RelaxData, RelaxErr, tauc, OtherInputs, magfield, Increment, FieldList, Delays, SetUp, B0LFields, PropFunction)
 
 
 #################################################### nWalker check ####################################################
@@ -117,16 +109,18 @@ def nWalkerCheck(nWalkers_b, nParam):
 
 
 #################################################### MCMC ####################################################
-def MarkovChainMonteCarlo(CorrDirName, TrajDirName, IntensityRelaxometry, RelaxData, B0HF, tauc, OtherInputs, magfield, Increment, FieldListUp, FieldListDown, ExperimentNumber, WTHF, d25, LFtimes, B0LFields, nWalkers, nMcmc, Bonds, labels, AA, PropFunction, nParam):
+def MarkovChainMonteCarlo(pdf_traj, pdf_corr, AA, nParam, CorrDirName, TrajDirName, tauc, magfield, nWalkers, nMcmc, Bonds, labels,
+                          Increment, PropFunction, shuttling_fields, shuttling_delays, 
+                          IntensityRelaxometry, ErrRelaxometry, RelaxData, RelaxErr, OtherInputs, set_up, B0LFields):
     
-    Labels = [[] for p in range(nParam)]
-    for i in range(len(labels)):
-        Labels[i] = labels[i]
-    Labels[-1] = "f"
+    Labels = []
+    for l in labels:
+        Labels.append(l)
+    Labels.append("f")
 
     Bonds.append([-10.0, 1.0])
     
-    lim = min(int(0.2*nMcmc), 500)
+    cutoff = min(int(0.2*nMcmc), 500)
 
     pos = []
     for w in range(nWalkers):
@@ -137,15 +131,25 @@ def MarkovChainMonteCarlo(CorrDirName, TrajDirName, IntensityRelaxometry, RelaxD
         pos.append(toadd)
     pos = np.array(pos)
     
-    
     with Pool() as pool:
-        sampler = emcee.EnsembleSampler(nWalkers, nParam, lnprob, args=(IntensityRelaxometry, RelaxData, B0HF, tauc, OtherInputs, magfield, Increment, FieldListUp, FieldListDown, ExperimentNumber, WTHF, d25, LFtimes, B0LFields, PropFunction), pool = pool)
-
-        sampler.run_mcmc(pos, nMcmc, progress=True);
+        sampler = emcee.EnsembleSampler(nWalkers, nParam, lnprob,
+                                        args=(AA, IntensityRelaxometry, ErrRelaxometry, RelaxData, RelaxErr, tauc, OtherInputs, magfield, Increment,
+                                              shuttling_fields, shuttling_delays, set_up, B0LFields, PropFunction),
+                                        pool = pool)
+        try:
+            sampler.run_mcmc(pos, nMcmc, progress=True);
+        except:
+            try:
+                width = 30
+                for i, result in enumerate(sampler.sample(pos, iterations=nMcmc)):
+                    n = int((width+1) * float(i) / nMcmc)
+                    sys.stdout.write("\r[{0}{1}]".format('#' * n, ' ' * (width - n)))
+                sys.stdout.write("\n")
+            except:
+                sampler.run_mcmc(pos, nMcmc);
     
-    samples = sampler.chain[:, lim:, :].reshape((-1, nParam))
+    samples = sampler.chain[:, cutoff:, :].reshape((-1, nParam))
     FullSamples = sampler.chain[:, :, :].reshape((-1, nParam))
-            
             
     Mean = np.percentile(samples, 50, axis=0)
     Sixteenth = np.percentile(samples, 16, axis=0)
@@ -161,13 +165,13 @@ def MarkovChainMonteCarlo(CorrDirName, TrajDirName, IntensityRelaxometry, RelaxD
     
     print("")
     for P in range(nParam):
-        print(Labels[P], Mean[P], PosError[P], NegError[P])
+        print(Labels[P], '\t', '{:.3e}'.format(Mean[P]), '\t', '{:.3e}'.format(PosError[P]), '\t', '{:.3e}'.format(NegError[P]))
     print("")
-    print("Mean Acceptance Fracttion: ", MAF)
+    print(f"Mean Acceptance Fracttion: {round(MAF, 4)}")
     
 
-    FigOut.FigTraj(TrajDirName, sampler, Mean, Labels, lim, AA)
-    FigOut.FigCorr(CorrDirName, samples, Mean, Labels, AA)
+    FigOut.FigTraj(pdf_traj, TrajDirName, sampler, Mean, Labels, cutoff, AA)
+    FigOut.FigCorr(pdf_corr, CorrDirName, samples, Mean, Labels, AA)
             
     return [Mean, PosError, NegError], MAF, FullSamples
 
