a98eb11a8ea7c59ec1bebb49440b1cd069df0113
nbolikcoulon <73346412+nbolikcoulon@users.noreply.github.com>
2024-05-23 14:19:11 -0400

    Add files via upload

---
+++ Demo/CHD2_EMF_Scale/Results/InputFiles/Expressions_and_constraints/Rates.h
@@ -0,0 +1,14 @@
+struct mat2d {
+
+    double m[3][3];
+
+};
+
+
+
+struct mat2d RelaxMatrix(double Bo, double *X, double *Tauc, double *OtherInputs );
+
+double R2calculation(double Bo, double *X, double *Tauc, double *OtherInputs );
+double R1calculation(double Bo, double *X, double *Tauc, double *OtherInputs );
+double Sigmacalculation(double Bo, double *X, double *Tauc, double *OtherInputs );
+double R1Hcalculation(double Bo, double *X, double *Tauc, double *OtherInputs );
