7922a111e6abda45f57be59f1c2fbb136ab6bb56
nbolikcoulon <73346412+nbolikcoulon@users.noreply.github.com>
2024-05-23 14:12:55 -0400

    Add files via upload

---
+++ Demo/CHD2_Explicit_NoScale/Results/InputFiles/Expressions_and_constraints/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy
+from setuptools import setup, Extension
+import os
+
+
+CurrentDirectiory = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    ext_modules=[Extension(CurrentDirectiory + "/_RelaxMat", [CurrentDirectiory + "/_Rates.c", CurrentDirectiory + "/Rates.c"])],
+    include_dirs=numpy.get_include(),
+)
+
+setup(
+    ext_modules=[Extension(CurrentDirectiory + "/Rates/_R1calculation", [CurrentDirectiory + "/_Rates.c", CurrentDirectiory + "/Rates.c"])],
+    include_dirs=numpy.get_include(),
+)
+
+setup(
+    ext_modules=[Extension(CurrentDirectiory + "/Rates/_R2calculation", [CurrentDirectiory + "/_Rates.c", CurrentDirectiory + "/Rates.c"])],
+    include_dirs=numpy.get_include(),
+)
+
+setup(
+    ext_modules=[Extension(CurrentDirectiory + "/Rates/_Sigmacalculation", [CurrentDirectiory + "/_Rates.c", CurrentDirectiory + "/Rates.c"])],
+    include_dirs=numpy.get_include(),
+)
+
+setup(
+    ext_modules=[Extension(CurrentDirectiory + "/Rates/_EtaZcalculation", [CurrentDirectiory + "/_Rates.c", CurrentDirectiory + "/Rates.c"])],
+    include_dirs=numpy.get_include(),
+)
+
+setup(
+    ext_modules=[Extension(CurrentDirectiory + "/Rates/_EtaXYcalculation", [CurrentDirectiory + "/_Rates.c", CurrentDirectiory + "/Rates.c"])],
+    include_dirs=numpy.get_include(),
+)
+
+
+#run python setup.py build_ext --inplace
